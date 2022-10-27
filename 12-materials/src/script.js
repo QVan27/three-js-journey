f982efabccef54b24f21e3e1ccad2455a65de4f8
QVan27 <qvannarath@gmail.com>
2022-10-27 18:42:57 +0200

    📝 Adding a few lights

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -66,7 +66,7 @@ const scene = new THREE.Scene();
 // const material = new THREE.MeshMatcapMaterial()
 // material.matcap = matcapTexture
 
-const material = new THREE.MeshDepthMaterial()
+const material = new THREE.MeshDepthMaterial();
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
@@ -80,6 +80,18 @@ const torus = new THREE.Mesh(
 torus.position.x = 1.5;
 
 scene.add(sphere, plane, torus);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
 
 /**
  * Sizes
