6a01ae8a16844d63ccda7a3b3accc1d24df455b1
QVan27 <qvannarath@gmail.com>
2022-10-27 11:09:14 +0200

    ✅ Debug UI

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -2,7 +2,35 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import gsap from "gsap";
+import GUI from "lil-gui"; // Import the GUI library
 
+console.log(GUI);
+
+/**
+ * Debug
+ */
+const gui = new GUI({ width: 150 }); // Create the GUI
+gui.open(gui._closed);
+// Implement h to hide the GUI
+window.addEventListener("keydown", (e) => {
+  if (e.key === "h") {
+    if (gui._hidden) gui.show();
+    else gui.hide();
+  }
+});
+
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 2, y: mesh.rotation.y + Math.PI * 2 });
+  },
+};
+
+gui.addColor(parameters, "color").onChange(() => {
+  material.color.set(parameters.color);
+});
+
+gui.add(parameters, "spin");
 /**
  * Base
  */
@@ -16,10 +44,14 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Debug
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+gui.add(mesh, "visible");
+gui.add(material, "wireframe");
 /**
  * Sizes
  */
